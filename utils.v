55c7d3693e6de0b6925ebde3d10106f9134d86af
Kazuhiro Haraguchi <arx.solid@gmail.com>
2021-11-19 12:40:07 +0900

    refactor is_wsl and remove choose_execute

--- utils.v
+++ utils.v
@@ -3,11 +3,6 @@ module utils
 import os
 import json
 
-pub const (
-	cmd_exe    = 'cmd.exe'
-	powershell = 'powershell.exe'
-)
-
 pub fn is_wsl() bool {
 	uname := os.uname()
 	if uname.sysname != 'Linux' {
@@ -15,17 +10,8 @@ pub fn is_wsl() bool {
 	}
 	if uname.release.contains('microsoft') {
 		return true
-	} else {
-		return false
 	}
-}
-
-pub fn choose_execute() ?string {
-	if is_wsl() {
-		return '$utils.cmd_exe /c start '
-	} else {
-		return error('System is not wsl.')
-	}
+	return false
 }
 
 pub fn load_json<T>(path string) ?T {
