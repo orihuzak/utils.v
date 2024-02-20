f3e6f3ffc58cd869d08ae92b1a8629ee9bde59de
rakaz <rakazv@outlook.com>
2024-02-20 17:50:24 +0900

    feat: add functions for xdg base directory

---
+++ src/xdg.v
@@ -0,0 +1,32 @@
+module vos
+
+// XDG Base Directory Specification
+import os
+
+// XDG_CONFIG_HOME use os.config_dir
+// XDG_CACHE_HOME use os.cache_dir
+
+pub fn xdg_data_home() string {
+	// TODO: fix it when v bug is fixed
+	dir := os.getenv_opt('XDG_DATA_HOME') or { os.join_path(os.home_dir(), '.local', 'share') }
+	return dir
+}
+
+pub fn xdg_state_home() string {
+	dir := os.getenv_opt('XDG_STATE_HOME') or { os.join_path(os.home_dir(), '.local', 'state') }
+	return dir
+}
+
+pub fn xdg_data_dirs() []string {
+	dirs := os.getenv_opt('XDG_DATA_DIRS') or { '/usr/local/share:/usr/share' }
+	return dirs.split(':')
+}
+
+pub fn xdg_config_dirs() []string {
+	dirs := os.getenv_opt('XDG_CONFIG_DIRS') or { '/etc/xdg' }
+	return dirs.split(':')
+}
+
+pub fn user_exe_dir() string {
+	return os.join_path(os.home_dir(), '.local', 'bin')
+}
---
+++ src/xdg_test.v
@@ -0,0 +1,13 @@
+module vos
+
+import os
+
+fn test_xdg_dirs() {
+	assert os.config_dir()! == os.join_path(os.home_dir(), '.config')
+	assert os.cache_dir() == os.join_path(os.home_dir(), '.cache')
+	assert xdg_data_home() == os.join_path(os.home_dir(), '.local', 'share')
+	assert xdg_state_home() == os.join_path(os.home_dir(), '.local', 'state')
+	assert xdg_data_dirs() == ['/usr/local/share', '/usr/share']
+	assert xdg_config_dirs() == ['/etc/xdg']
+	assert user_exe_dir() == os.join_path(os.home_dir(), '.local', 'bin')
+}
